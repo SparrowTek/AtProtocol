a4e77a96b30809559a22dfa0a6fe963f28402114
Thomas Rademaker <thomas@sparrowtek.com>
2023-09-05 15:31:22 -0400

    give the option to setup everything needed

--- Sources/AtProtocol/AtProtocol.swift
+++ Sources/AtProtocol/AtProtocol.swift
@@ -1,3 +1,8 @@
+public func setup(hostURL: String, userSession: Session) {
+    host = hostURL
+    session = userSession
+}
+
 public func update(userSession: Session?) {
     session = userSession
 }
