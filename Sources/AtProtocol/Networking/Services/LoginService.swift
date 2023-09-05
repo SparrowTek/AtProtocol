06a76d73cbf43e876251ab6cd9708a149a6672cc
Thomas Rademaker <thomas@sparrowtek.com>
2023-09-05 14:01:30 -0400

    create a new apiClient each time a request is made

--- Sources/AtProtocol/Networking/Services/LoginService.swift
+++ Sources/AtProtocol/Networking/Services/LoginService.swift
@@ -1,12 +1,10 @@
 import Get
 
 public struct LoginService {
-    private var apiClient: APIClient { APIClient(configuration: configuration) }
-    
     public init() {}
     
     public func login(identifier: String, password: String) async throws -> Session {
         let loginObject = LoginObject(identifier: identifier, password: password)
-        return try await apiClient.send(Request(path: "/xrpc/com.atproto.server.createSession", method: .post, body: loginObject)).value
+        return try await APIClient(configuration: configuration).send(Request(path: "/xrpc/com.atproto.server.createSession", method: .post, body: loginObject)).value
     }
 }
