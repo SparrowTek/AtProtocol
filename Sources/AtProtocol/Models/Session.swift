37c87d13373d5e6430f8b13aeeae90b98e19f694
Thomas Rademaker <thomas@sparrowtek.com>
2023-09-05 15:38:51 -0400

    give session a public init

--- Sources/AtProtocol/Models/Session.swift
+++ Sources/AtProtocol/Models/Session.swift
@@ -4,4 +4,12 @@ public struct Session: Codable {
     public let email: String
     public let accessJwt: String
     public let refreshJwt: String
+    
+    public init(did: String, handle: String, email: String, accessJwt: String, refreshJwt: String) {
+        self.did = did
+        self.handle = handle
+        self.email = email
+        self.accessJwt = accessJwt
+        self.refreshJwt = refreshJwt
+    }
 }
