7b20ca331cb369484394701304556b88de386b9a
Thomas Rademaker <thomas@sparrowtek.com>
2023-09-16 09:01:54 -0400

    everything in viewer needs to be optional

--- Sources/AtProtocol/Models/Viewer.swift
+++ Sources/AtProtocol/Models/Viewer.swift
@@ -1,10 +1,10 @@
 public struct Viewer: Codable {
-    public var muted: Bool
-    public var blockedBy: Bool
+    public var muted: Bool?
+    public var blockedBy: Bool?
     public var following: String?
     public var followedBy: String?
     
-    public init(muted: Bool, blockedBy: Bool, following: String?, followedBy: String?) {
+    public init(muted: Bool?, blockedBy: Bool?, following: String?, followedBy: String?) {
         self.muted = muted
         self.blockedBy = blockedBy
         self.following = following
