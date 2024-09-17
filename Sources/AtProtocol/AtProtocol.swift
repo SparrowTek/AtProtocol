edf05a70a410908f3cd21cb4d32cdb012896363b
Thomas Rademaker <thomas@radmakr.com>
2024-09-17 06:04:56 -0400

    bring back top level functions

--- Sources/AtProtocol/AtProtocol.swift
+++ Sources/AtProtocol/AtProtocol.swift
@@ -2,3 +2,27 @@
 public protocol ATProtocolDelegate: AnyObject {
     func sessionUpdated(_ session: Session) async
 }
+
+@APActor
+public func setup(hostURL: String?, accessJWT: String?, refreshJWT: String?, delegate: ATProtocolDelegate? = nil) {
+    APEnvironment.current.host = hostURL
+    APEnvironment.current.accessToken = accessJWT
+    APEnvironment.current.refreshToken = refreshJWT
+    APEnvironment.current.atProtocoldelegate = delegate
+}
+
+@APActor
+public func setDelegate(_ delegate: ATProtocolDelegate) {
+    APEnvironment.current.atProtocoldelegate = delegate
+}
+
+@APActor
+public func updateTokens(access: String?, refresh: String?) {
+    APEnvironment.current.accessToken = access
+    APEnvironment.current.refreshToken = refresh
+}
+
+@APActor
+public func update(hostURL: String?) {
+    APEnvironment.current.host = hostURL
+}
